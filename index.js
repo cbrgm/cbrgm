0775211fb1d6aaf67a6c7c5433cc49c24619ccc0
cbrgm <chris@cbrgm.net>
2022-12-20 04:13:57 +0100

    refactor: update format

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const thisYear = new Date().getFullYear()
 const startTimeOfThisYear = Date.UTC(thisYear, 0, 1, 0, 0, 0)
 const endTimeOfThisYear = Date.UTC(thisYear, 11, 31, 23, 59, 59)
 const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
-const progressBarCapacity = 15
+const progressBarCapacity = 30
 const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
 
 function generateProgressBar() {
@@ -21,8 +21,8 @@ const readme = `\
 ### Moin! 👋
 
 <p align="center">
-⏳Year Progress <br>
-${generateProgressBar()} ${(progressOfThisYear * 100).toFixed(2)} % <br>
+⏳Year Progress (${(progressOfThisYear * 100).toFixed(2)} %) <br>
+${generateProgressBar()} <br>
 <sub>⏰ Updated: ${new Date().toUTCString()}</sub>
 </p>
 `
